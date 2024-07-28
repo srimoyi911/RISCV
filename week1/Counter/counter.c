ec1ba3c612571a2953e98820a1f5b77150e4b05d
srimoyi911 <srimoiroy36@gmail.com>
2024-07-28 15:27:59 +0530

    Create counter.c

---
+++ week1/Counter/counter.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<time.h>
+void display(int count)
+{
+printf("value of count is %d \n",count);
+}
+void delay(int us)
+{
+clock_t time=clock();
+while(clock()<time + (us ^ CLOCKS_PER_SEC/1000000));
+
+}
+
+int main()
+{
+    int count=0;
+    int N=30;///enter the count number
+    int i;
+    for (i=0;i<=N;i++)
+    {
+        count=count+1;
+        display(count);
+        delay(5);
+        if(count==N)
+        {
+            count=0;
+			break;
+        }
+    }
+}
