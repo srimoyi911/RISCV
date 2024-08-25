a995f75a447990cbdb40d1fbe11310e206b3ab4b
srimoyi911 <srimoiroy36@gmail.com>
2024-08-25 23:32:16 +0530

    Update Sitting_posture_detection_inline_asm.c

--- week4/Sitting_posture_detection_inline_asm.c
+++ week4/Sitting_posture_detection_inline_asm.c
@@ -1,14 +1,24 @@
+int division(int dividend, int divisor) {
+    int quotient = 0;
+    
+   
+    while (dividend >= divisor) {
+        dividend -= divisor;
+        quotient++;
+    }
+
+    return quotient;
+}
 
 int main() {
 
-	float dist_hd,distance_calc, dist_back;
-	int trigger,temp, buzzer,led;
+  int freq=100; //100Hz
   // Get the 4 bit distance input value from a potentiometer  and store it into variable //check
   //X30[3:0]=distance input pin
   int dist_hd_back;
-  int clear_gp0_mask=0xFFFFFFF0;
+  int clear_gp0_mask=0x0000000F;
   asm volatile (
-    "and x30,x30,%0\n\t" //keeping only the 4 bit LSB enabled and masking others
+    "and x30,x30,%0\n\t" //passing only the 4 bit LSB enabled and masking others
     "addi %0,x30, 0\n\t" //x30 value is put into dist_hd_back variable
     :"r"(clear_gp0_mask),
     :"=r"(dist_hd_back)
@@ -17,7 +27,7 @@ int main() {
   //get the 4bit input timeout value from potentiometer  and store it into variable //check
   //X30[7:4]=timeout input pin
    int timeout;
-   int clear_gp1_mask=0xFFFFFF0F;
+   int clear_gp1_mask=0x000000F0;
    asm volatile (
     "and x30,x30,%0\n\t" 
     "and x10, x10,0\n\t"
@@ -34,26 +44,41 @@ int main() {
    int trigger_write=trigger<<8;
    int clear_gp2_mask=0xFFFFFCFF;
    asm volatile (
-    "and x30,x30,%0\n\t" 
-    "or x30,x30,%1\n\t" //x10 value is put into timeout variable
+    "and x30,x30,%0\n\t" //clearing the required bits from junk value
+    "or x30,x30,%1\n\t" //trigger write var is put into x30 bits 8,9
     :"r"(clear_gp2_mask),"r"(trigger_write)
   );
   int i;
   for(i=0;i<12;i++); //trigger ON for 12usec
 	
-  //trigger low for two sensors
+  //trigger out low for two sensors
   //X30[9:8]= trigger output pin
   asm volatile (
     "and x30,x30,%0\n\t"  //clearing the trigger pins of x30
     :"r"(clear_gp2_mask)
   );
 
-  
-
   //get the distance values from two sensors and store it into temp1 and temp2 variables
   //x30[11:10]=high output coming from two sensors
-     
-
+    int  echo;
+    int clear_gp3_mask=0x00000C00;
+    asm volatile (
+    "and x30,x30,%0\n\t" //keeping only the 10,11 bit  enabled and masking others
+    "srli x30,x30,10\n\t"
+    "addi %0,x30, 0\n\t" //x30 value is put into dist_hd_back variable
+    :"r"(clear_gp3mask),
+    :"=r"(echo)
+  ); 
+  int count=0;
+  int distance=0;
+  if (echo >=1 && echo <=3)
+  {
+          printf("echo triggered");
+	  count=count+1;
+  } 
+  duration=division(count,freq); //returns the value in sec
+  distance = duration*172; //distance is in meter
+  if
     while(1){
     //debug
 	echo_hd=1;
